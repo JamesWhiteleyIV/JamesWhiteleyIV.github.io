e868ee1a0c9deb2edce05871c91dce67cdb07a12
James Whiteley <jameswhiteley0@gmail.com>
2019-08-19 11:58:34 -0700

    update

--- js/index.js
+++ js/index.js
@@ -11,10 +11,11 @@ linkRequest.onreadystatechange = function () {
             var html = "";
             JSON.parse(linkRequest.responseText).forEach(function (link) {
                 console.log(link.name);
-                if (link.name == "Resume"):
+                if (link.name == "Resume") {
                     html += "<div class=\"links waves-effect\"><a href=" + link.url + " class=\"black-text valign-wrapper\" download><i class=\"link_icon mdi mdi-" + link.icon + "\" style=\"color: " + link.color + "\"></i>&nbsp; " + link.name + "</a></div>";
-                else:
+                } else {
                     html += "<div class=\"links waves-effect\"><a href=" + link.url + " class=\"black-text valign-wrapper\" target=\"_blank\"><i class=\"link_icon mdi mdi-" + link.icon + "\" style=\"color: " + link.color + "\"></i>&nbsp; " + link.name + "</a></div>";
+                }
             });
             link.innerHTML = html;
         }
